b8cc4f3b1dd581552f6860db0d3a718ac25c8d1d
Akash Kumar <akashsinghsharmaa@gmail.com>
2024-08-04 16:51:49 +0530

    basic webpage added

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-# This is my code base for portfolio project
+import streamlit as st
 
+st.set_page_config(layout= "wide")
+
+col1 , col2 = st.columns(2)
+
+with col1:
+    st.image("IMAGES/photo.png")
+
+with col2:
+    st.title("Akash Kumar")
+    content = """
+    I am a Data professional """
+    st.info(content)
